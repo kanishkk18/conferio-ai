73ba60d4c7613f9e437d842b319c99b0b6d5668d
kanishkk Bansal <168306760+kanishkk18@users.noreply.github.com>
2026-02-22 23:33:14 +0530

    Update loading.tsx

--- src/app/[variants]/(main)/chat/loading.tsx
+++ src/app/[variants]/(main)/chat/loading.tsx
@@ -1,3 +1,5 @@
-import Loading from '@/components/Loading/BrandTextLoading';
 
-export default () => <Loading />;
+export default () => {
+    return( <div>
+        <video autoPlay loop muted className='h-screen w-screen max-h-screen max-w-screen object-cover ' src="https://res.cloudinary.com/kanishkkcloud18/video/upload/v1763123553/rocket_launch_keimtd.mp4"/>
+      </div>)};
